193a550afab1162a491c7f0326ae3f22303218e9
Matija Dodovic <matija.dodovic@gmail.com>
2024-04-08 16:30:35 +0000

    initial

---
+++ omp/nbody/nbody.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <omp.h> 
+
+#define DIM 2  /* Two-dimensional system */
+#define X 0    /* x-coordinate subscript */
+#define Y 1    /* y-coordinate subscript */
+
+const double G = 6.673e-11;  
+
+typedef double vect_t[DIM];  /* Vector type for position, etc. */
+
+struct particle_s {
+   double m;  /* Mass     */
+   vect_t s;  /* Position */
+   vect_t v;  /* Velocity */
+};
+
+void Get_args(int argc, char* argv[], int* n_p, int* n_steps_p, double* delta_t_p, int* output_freq_p, char* g_i_p);
+void Get_init_cond(struct particle_s curr[], int n);
+void Gen_init_cond(struct particle_s curr[], int n);
+void Compute_force(int part, vect_t forces[], struct particle_s curr[], int n);
+void Update_part(int part, vect_t forces[], struct particle_s curr[], int n, double delta_t);
+void Compute_energy(struct particle_s curr[], int n, double* kin_en_p, double* pot_en_p);
+
+int main(int argc, char* argv[]) {
+   int n;                      /* Number of particles        */
+   int n_steps;                /* Number of timesteps        */
+   int step;                   /* Current step               */
+   int part;                   /* Current particle           */
+   int output_freq;            /* Frequency of output        */
+   double delta_t;             /* Size of timestep           */
+   double t;                   /* Current Time               */
+   struct particle_s* curr;    /* Current state of system    */
+   vect_t* forces;             /* Forces on each particle    */
+   char g_i;                   /*_G_en or _i_nput init conds */
+   double kinetic_energy, potential_energy;
+   double start, finish;       /* For timings                */
+
+   Get_args(argc, argv, &n, &n_steps, &delta_t, &output_freq, &g_i);
+   curr = malloc(n*sizeof(struct particle_s));
+   forces = malloc(n*sizeof(vect_t));
+   if (g_i == 'i')
+      Get_init_cond(curr, n);
+   else
+      Gen_init_cond(curr, n);
+
+   int num_threads = omp_get_max_threads();
+   printf("Number of threads: %d\n", num_threads);
+
+
+   double time1, time2, elapsed;
+   time1 = omp_get_wtime();
+
+
+   Compute_energy(curr, n, &kinetic_energy, &potential_energy);
+   for (step = 1; step <= n_steps; step++) {
+      t = step*delta_t;
+      memset(forces, 0, n*sizeof(vect_t));
+
+#pragma omp parallel for default(none) shared(forces, curr, n) private(k, f_part_k, len, len_3, fact, mg)
+      for (part = 0; part < n-1; part++)
+         Compute_force(part, forces, curr, n);
+
+#pragma omp parallel for default(none) shared(curr, forces, delta_t, n) private(fact)
+      for (part = 0; part < n; part++)
+         Update_part(part, forces, curr, n, delta_t);
+
+      Compute_energy(curr, n, &kinetic_energy, &potential_energy);
+   }
+   printf("PE = %e, KE = %e, Total Energy = %e\n", potential_energy, kinetic_energy, kinetic_energy+potential_energy);
+
+   time2 = omp_get_wtime();
+   elapsed = time2 - time1;
+   printf("Time elapsed: %f seconds\n", elapsed);
+   printf("---------------------------------------\n\n");
+
+   free(curr);
+   free(forces);
+   return 0;
+}  /* main */
+
+void Get_args(int argc, char* argv[], int* n_p, int* n_steps_p, 
+      double* delta_t_p, int* output_freq_p, char* g_i_p) {
+   *n_p = strtol(argv[1], NULL, 10);
+   *n_steps_p = strtol(argv[2], NULL, 10);
+   *delta_t_p = strtod(argv[3], NULL);
+   *output_freq_p = strtol(argv[4], NULL, 10);
+   *g_i_p = argv[5][0];
+
+}  /* Get_args */
+
+void Get_init_cond(struct particle_s curr[], int n) {
+   int part;
+
+   for (part = 0; part < n; part++) {
+      scanf("%lf", &curr[part].m);
+      scanf("%lf", &curr[part].s[X]);
+      scanf("%lf", &curr[part].s[Y]);
+      scanf("%lf", &curr[part].v[X]);
+      scanf("%lf", &curr[part].v[Y]);
+   }
+}  /* Get_init_cond */
+
+void Gen_init_cond(struct particle_s curr[], int n) {
+   int part;
+   double mass = 5.0e24;
+   double gap = 1.0e5;
+   double speed = 3.0e4;
+
+   srandom(1);
+   for (part = 0; part < n; part++) {
+      curr[part].m = mass;
+      curr[part].s[X] = part*gap;
+      curr[part].s[Y] = 0.0;
+      curr[part].v[X] = 0.0;
+      if (part % 2 == 0)
+         curr[part].v[Y] = speed;
+      else
+         curr[part].v[Y] = -speed;
+   }
+}  /* Gen_init_cond */
+
+void Compute_force(int part, vect_t forces[], struct particle_s curr[], 
+      int n) {
+   int k;
+   double mg; 
+   vect_t f_part_k;
+   double len, len_3, fact;
+
+   for (k = part+1; k < n; k++) {
+      f_part_k[X] = curr[part].s[X] - curr[k].s[X];
+      f_part_k[Y] = curr[part].s[Y] - curr[k].s[Y];
+      len = sqrt(f_part_k[X]*f_part_k[X] + f_part_k[Y]*f_part_k[Y]);
+      len_3 = len*len*len;
+      mg = -G*curr[part].m*curr[k].m;
+      fact = mg/len_3;
+      f_part_k[X] *= fact;
+      f_part_k[Y] *= fact;
+
+      forces[part][X] += f_part_k[X];
+      forces[part][Y] += f_part_k[Y];
+      forces[k][X] -= f_part_k[X];
+      forces[k][Y] -= f_part_k[Y];
+   }
+}  /* Compute_force */
+
+void Update_part(int part, vect_t forces[], struct particle_s curr[], 
+      int n, double delta_t) {
+   double fact = delta_t/curr[part].m;
+
+   curr[part].s[X] += delta_t * curr[part].v[X];
+   curr[part].s[Y] += delta_t * curr[part].v[Y];
+   curr[part].v[X] += fact * forces[part][X];
+   curr[part].v[Y] += fact * forces[part][Y];
+}  /* Update_part */
+
+void Compute_energy(struct particle_s curr[], int n, double* kin_en_p,
+      double* pot_en_p) {
+   int i, j;
+   vect_t diff;
+   double pe = 0.0, ke = 0.0;
+   double dist, speed_sqr;
+
+#pragma omp parallel for default(none) shared(curr, n) private(speed_sqr) reduction(+:ke)
+   for (i = 0; i < n; i++) {
+      speed_sqr = curr[i].v[X]*curr[i].v[X] + curr[i].v[Y]*curr[i].v[Y];
+      ke += curr[i].m*speed_sqr;
+   }
+   ke *= 0.5;
+
+#pragma omp parallel for default(none) shared(curr, n) private(diff, dist) reduction(+:pe) collapse(2)
+   for (i = 0; i < n-1; i++) {
+      for (j = i+1; j < n; j++) {
+         diff[X] = curr[i].s[X] - curr[j].s[X];
+         diff[Y] = curr[i].s[Y] - curr[j].s[Y];
+         dist = sqrt(diff[X]*diff[X] + diff[Y]*diff[Y]);
+         pe += -G*curr[i].m*curr[j].m/dist;
+      }
+   }
+
+   *kin_en_p = ke;
+   *pot_en_p = pe;
+}  /* Compute_energy */
