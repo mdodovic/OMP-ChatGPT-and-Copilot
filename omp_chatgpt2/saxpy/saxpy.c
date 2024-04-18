031f68e31db3327b2b675fb864f7080163dc4f27
Matija Dodovic <matija.dodovic@gmail.com>
2024-04-18 21:57:51 +0000

    saxpy

--- omp_chatgpt2/saxpy/saxpy.c
+++ omp_chatgpt2/saxpy/saxpy.c
@@ -30,6 +30,7 @@ THE SOFTWARE.
 #define NTIMERS 3
 
 void saxpy(int n, float a, float *restrict x, float *restrict y, float *restrict z) {
+      # pragma omp parallel for
         for (int i = 0; i < n; i++)
                 z[i] = a*x[i] + y[i];
 }
